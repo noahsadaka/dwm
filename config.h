d1302378c4b34b7e37bdd671c4f58c1a1f1bc4c2
Noah Sadaka <noahsadaka@gmail.com>
2021-01-14 14:57:08 -0500

    quit without restarting via alt+shift+q

--- config.h
+++ config.h
@@ -93,7 +93,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+/*	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },*/
+	{ MODKEY|ShiftMask, XK_q,      quit,           {1} }, 
 };
 
 /* button definitions */
